9fde9644da9750411c9d27083393ca6e6becc36a
Seth Wax <sethlwax@gmail.com>
2018-03-27 18:14:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each{|dwarf| puts dwarf}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map(|call| call.capitalize + '!')
 end
 
 def long_planeteer_calls# code an argument here
