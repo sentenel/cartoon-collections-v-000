70a22c2a565158eaeb43d74c482afea6c2c78cc1
Seth Wax <sethlwax@gmail.com>
2018-03-27 18:23:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(words)
   words.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.find{|string| cheese_types.include?(string)}
 end
